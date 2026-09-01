50c9a77a42254caadf05065a517e4b1e3314f272
nicholasfocke <128650811+nicholasfocke@users.noreply.github.com>
2026-09-01 10:40:30 -0300

    feat: implementation of BST and graphs

--- Files/exercise/UdemyDataStructureAndAlg/BST.py
+++ Files/exercise/UdemyDataStructureAndAlg/BST.py
@@ -47,18 +47,87 @@ class BinarySearchTree:
                 return True
             
         return False
-        
+
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent(self, index):
+        return (index - 1) // 2 
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+
+        return max_value
+
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            if (left_index < len(self.heap) and self.heap[left_index] > self.heap[max_index]):
+                max_index = left_index
+
+            if (right_index < len(self.heap) and self.heap[right_index] > self.heap[max_index]):
+                max_index = right_index
+
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
 
 
-my_bst = BinarySearchTree()
-my_bst.insert(47)
-my_bst.insert(21)
-my_bst.insert(76)
-my_bst.insert(18)
-my_bst.insert(27)
-my_bst.insert(52)
-my_bst.insert(82)
 
-print(my_bst.contains(10))
-print(my_bst.contains(82))
+# my_bst = BinarySearchTree()
+# my_bst.insert(47)
+# my_bst.insert(21)
+# my_bst.insert(76)
+# my_bst.insert(18)
+# my_bst.insert(27)
+# my_bst.insert(52)
+# my_bst.insert(82)
 
+# print(my_bst.contains(10))
+# print(my_bst.contains(82))
+
+
+myheap = MaxHeap()
+myheap.insert(99)
+myheap.insert(72)
+myheap.insert(61)
+myheap.insert(58)
+
+print(myheap.heap)
+
+myheap.insert(100)
+print(myheap.heap)
+
+myheap.insert(75)
+print(myheap.heap)
